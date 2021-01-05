ad0a08b547347402651eb76c39396eb56058ebc6
Michał Poręba <michalporeba@gmail.com>
2021-01-05 19:23:14 +0000

    fixing the install.ps1

--- src/dbapipes.psm1
+++ src/dbapipes.psm1
@@ -1,7 +1,5 @@
-. .\functions\Compare-DbpQuery.ps1
-. .\functions\Get-DbpPackage.ps1
-. .\functions\Get-DbpTask.ps1
+. $PSScriptRoot\functions\Get-DbpPackage.ps1
+. $PSScriptRoot\functions\Get-DbpTask.ps1
 
-Export-ModuleMember -Function Compare-DbpQuery
 Export-ModuleMember -Function Get-DbpPackage
 Export-ModuleMember -Function Get-DbpTask 
